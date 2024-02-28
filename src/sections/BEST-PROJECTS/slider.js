a984fb80940265dbc05ec8a04df278240304f5ae
Saysaw <g.o.developerue@gmail.com>
2024-02-28 10:59:57 +0500

    slider

--- src/sections/BEST-PROJECTS/slider.js
+++ src/sections/BEST-PROJECTS/slider.js
@@ -3,52 +3,25 @@ import Swiper from 'swiper';
 import 'swiper/css';
 
 
-document.addEventListener("DOMContentLoaded", function () {
-	const swiper = new Swiper('.swiper-container', { // Убедитесь, что здесь используется правильный селектор
-	  // Optional parameters
-	  direction: 'vertical',
+
+
+document.addEventListener("DOMContentLoaded", function() {
+	var swiper = new Swiper('.swiper', {
+	  // Параметры Swiper
 	  loop: true,
-  
-	  // If we need pagination
 	  pagination: {
 		el: '.swiper-pagination',
+		clickable: true,
 	  },
-  
-	  // Navigation arrows
 	  navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	  },
-  
-	  // And if we need scrollbar
-	  scrollbar: {
-		el: '.swiper-scrollbar',
+	  // Автопрокрутка
+	  autoplay: {
+		delay: 5000,
 	  },
 	});
-  }); // Убрана лишняя закрывающая скобка и точка с запятой
+  });
   
-
-
-
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-// 	console.log(123)
-// 	var swiper = new Swiper('.swiper-container', {
-// 	  // Параметры Swiper
-// 	  loop: true,
-// 	  pagination: {
-// 		el: '.swiper-pagination',
-// 		clickable: true,
-// 	  },
-// 	  navigation: {
-// 		nextEl: '.swiper-button-next',
-// 		prevEl: '.swiper-button-prev',
-// 	  },
-// 	  // Автопрокрутка
-// 	  autoplay: {
-// 		delay: 5000,
-// 	  },
-// 	});
-//   });
-
+  
