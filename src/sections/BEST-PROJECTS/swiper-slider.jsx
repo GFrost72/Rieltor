f986a595a093472602290c688b0b91fbb1f7cbad
Saysaw <g.o.developerue@gmail.com>
2024-02-29 11:06:58 +0500

    slider

--- src/sections/BEST-PROJECTS/swiper-slider.jsx
+++ src/sections/BEST-PROJECTS/swiper-slider.jsx
@@ -3,34 +3,35 @@ import { Navigation, Pagination } from 'swiper/modules';
 
 import 'swiper/css/bundle';
 
+
 const SwiperSlider = ({ slideSources }) => {
-    return (
-        <Swiper
-            loop
-            className="custom-slider"
-            navigation
-            modules={[Navigation, Pagination]}
-            spaceBetween={50}
-            slidesPerView={1}
-            pagination={{ clickable: true }}
-            scrollbar={{ draggable: true }}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
+  return (
+    <Swiper
+      loop
+      className="custom-slider"
+      navigation
+      modules={[Navigation, Pagination]}
+      spaceBetween={50}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      {slideSources.map((slideSource) => (
+        <SwiperSlide
+          className="swiper-custom-slide"
+          key={slideSource.src}
         >
-			
-            {
-                slideSources.map((slideSource) => (
-                    <SwiperSlide
-                        className="swiper-custom-slide"
-                        key={slideSource.src}
-                    >
-                        <img src={slideSource.src} alt={slideSource.alt} />
-                    </SwiperSlide>
-                ))
-            }
-        </Swiper>
-    );
+          <p className="slider-text">{slideSource.text}</p>
+          {/* Apply the dimming effect directly to the image */}
+          <div className="image-container">
+            <img src={slideSource.src} alt={slideSource.alt} className="dimmed-image"/>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 };
 
-
-export { SwiperSlider }
+export { SwiperSlider };
